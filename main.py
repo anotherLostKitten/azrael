d95d6570bf9159dd2fce2a62393a978999c132cd
jwongUlti25 <jwong25@stuy.edu>
2018-10-24 05:48:31 -0400

    check devlog.txt for progress

--- main.py
+++ main.py
@@ -3,121 +3,112 @@
 # P00 -- Da Art of Storytellin'
 
 import os
-import sqlite3
 
-from flask import Flask, redirect, url_for, render_template, request, flash, get_flashed_messages
-# import utils.db as dog
+from flask import Flask, redirect, url_for, render_template, session, request, flash, get_flashed_messages
+from utils import db as azrael
 
 DB_FILE = "data/azrael_stories.db"
-
-db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
-c = db.cursor()  # facilitate db ops
-
 app = Flask(__name__)
-
+user = None
 app.secret_key = os.urandom(32)
 
-accts = {}
-
-def droptable(tablename):
-    command = "DROP TABLE IF EXISTS {0};".format(tablename)
-    c.execute(command)
-
-def addrow(table,tup):
-    checky = c.execute("SELECT * FROM {tname};".format(tname = table))
-    is_in = tup in [x for x in checky]
-    if not is_in:
-        command = "INSERT INTO {0} VALUES(?, ?, ?)"
-        c.execute(command.format("users"), tup)
-        print("added", tup )
-        return;
-    print("did not add", tup)
-
-def cleartable(table):
-    command = "DELETE FROM {tname}"
-    c.execute(command.format(tname = table))
-
-#cleartable("users")
-
-addrow("users",("w", "z", 0))
-addrow("users",("w", "z", 0))
-addrow("users",("wjt", "zwa", 2))
-addrow("users",("wwww", "user", 2))
-
-# dog.insertRow('users', ('j', 't', 0))
-
-def reloadAccts():
-    test = c.execute("SELECT * FROM users;")
-    accts = {}
-    for x in test:
-        accts[str(x[0])] = {"pass": str(x[1]), "id": int(x[2])}
-    print("<==start dict==>")
-    print (accts)
-    print("<==end dict==>")
-
-reloadAccts()
-
-who  = {}
-
+def setUser(userName):
+    global user
+    user = userName
 
 @app.route('/')
-def hello_world():
-    return render_template("root.html")
+def home():
+    if user in session:
+        data = azrael.DB_Manager(DB_FILE)
+        userStories = sorted(data.getStoriesContributedTo(user))
+        return render_template('user.html', user_name = user, errors = False, stories = userStories)
+    return render_template("home.html", errors = False)
 
 @app.route('/login')
-def login_world():
-    return render_template("login.html")
+def login():
+    return render_template("login.html", errors = True)
 
 @app.route('/register')
-def register_world():
-    return render_template("register.html")
+def register():
+    return render_template("register.html", errors = True)
 
 @app.route('/auth', methods=['POST'])
 def auth():
-    #LOGGING IN MEN
-    if request.form["submit"] == "login":
-        if request.form["username"] in accts:
-            if str(request.form['password']) == accts[request.form["username"]]["pass"]:
-                who["username"] = request.form["username"]
-                return "u r men"
-            print(str(request.form['password']), accts[request.form["username"]])
-            flash('bad! not ur password bro')
+    # instantiates DB_Manager with path to DB_FILE
+    data = azrael.DB_Manager(DB_FILE)
+    username, password = request.form["username"], request.form['password']
+    # LOGGING IN
+    if request.form["submit"] == "Login":
+        if data.verifyUser(username, password ) :
+            session[username] = password
+            setUser(username)
+            data.save()
+            return redirect(url_for('home'))
+        # user was found in DB but password did not match
+        elif data.findUser(username):
+            flash('Incorrect password!')
+        # user not found in DB at all
         else:
-            flash('bad! username not here!')
-        #print("hello", get_flashed_messages())
-        return(render_template("login.html"))
-    #REGISTERING MEN
+            flash('Incorrect username!')
+        data.save()
+        return render_template("login.html", errors = True)
+    # REGISTERING
     else:
-        if len(request.form["username"]) > 0 and request.form["username"] not in accts:
-            if len(request.form['password']) > 0:
-                #add acct
-                addrow("users",request.form["username"],request.form['password'],2)
-            flash('bad! pass too short')
+        if len(username) > 0 and not data.findUser(username):
+            if len(password) > 0:
+                # add the account to DB
+                data.registerUser(username, password)
+                data.save()
+                return redirect(url_for('home'))
+            else:
+                flash('Bad password!')
         else:
-            flash('bad! username taken or maybe it\'s too short!')
-        # print("hello", get_flashed_messages())
-        return (render_template("register.html"))
+            flash("Username already taken!")
+        # TRY TO REGISTER AGAIN
+        data.save()
+        return render_template("register.html", errors = True)
 
-    # if request.form['username'] in acc:
-    #     if request.form['password'] == 'Tung':
-    #         session['Ahmed'] = 'Tung'
-    #         flash('Success! Logged in as Ahmed', 'success')
-    #         return render_template('yes.html',usr = 'Ahmed')
-    #     else:
-    #         flash('Incorrect Password!', 'error')
-    #         return render_template('login.html')
-    # flash('Incorrect Username!', 'error')
-    # return render_template('login.html')
+@app.route('/logout')
+def logout():
+    session.pop(user, None)
+    setUser(None)
+    return redirect(url_for('home'))
 
-@app.route('/home')
-def homer():
-    #something about getting cookies from register or login
-    return render_template("root.html")
+@app.route('/viewstory')
+def viewstory():
+    data = azrael.DB_Manager(DB_FILE)
+    storyTitle = request.args['submit']
+    content = data.getStoryText(storyTitle)
+    data.save()
+    return render_template('story.html', storyTitle = storyTitle, content = content)
+
+@app.route('/viewothers')
+def viewothers():
+    data = azrael.DB_Manager(DB_FILE)
+    pass
+
+@app.route('/appendToStory')
+def appendToStory():
+    data = azrael.DB_Manager(DB_FILE)
+    pass
+
+@app.route('/creator')
+def storyCreator():
+    return render_template('create.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    data = azrael.DB_Manager(DB_FILE)
+    story, line = request.form['title'], request.form['line']
+    id = data.getID_fromUser(user)
+    data.createStory(story)
+    data.addToStory(story, line, id)
+    data.save()
+    return render_template('story.html', storyTitle = story, content = line)
 
 
-app.debug = True
-app.run()
 
 
-db.commit()  # save changes
-db.close()  # close database
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
